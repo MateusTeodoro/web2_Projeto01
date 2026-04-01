2ffcb0646b49809535bfe73ef47af3c18b701edf
Mateus <mateustpinheiro@gmail.com>
2026-03-31 21:25:03 -0300

    Primeiro commit

---
+++ src/main/java/br/edu/iftm/prjweb2/Prjweb2Application.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.prjweb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prjweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prjweb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/prjweb2/Prjweb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.prjweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prjweb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
