baa207426a9b0bb33fc2416edcf305ec4e16fc6e
Mateus <mateustpinheiro@gmail.com>
2026-04-08 23:05:33 -0300

    Adição da funcionalidade excluir

--- src/main/java/br/edu/iftm/prjweb2/controller/KnifeController.java
+++ src/main/java/br/edu/iftm/prjweb2/controller/KnifeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import br.edu.iftm.prjweb2.model.Knife;
@@ -31,6 +32,12 @@ public class KnifeController {
     @PostMapping("/knife/save")
     public String postMethodName(@ModelAttribute("knife") Knife knife) {
         knifeService.saveKnife(knife);
+        return "redirect:/knife";
+    }
+
+    @GetMapping("/knife/delete/{id}")
+    public String delete(@PathVariable Long id) {
+        this.knifeService.deleteKnifeById(id);
         return "redirect:/knife";
     }
 }
