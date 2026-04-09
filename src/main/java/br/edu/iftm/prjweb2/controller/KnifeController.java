13c82d22940b203620420c07dde48c5af9795781
Mateus <mateustpinheiro@gmail.com>
2026-04-08 22:00:04 -0300

    Adição da funcionalidade cadastrar

--- src/main/java/br/edu/iftm/prjweb2/controller/KnifeController.java
+++ src/main/java/br/edu/iftm/prjweb2/controller/KnifeController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import br.edu.iftm.prjweb2.model.Knife;
 import br.edu.iftm.prjweb2.service.KnifeService;
 
 @Controller
@@ -17,5 +20,17 @@ public class KnifeController {
     public String index(Model model) {
         model.addAttribute("knifeList", knifeService.getAllKnifes());
         return "knife/index";
+    }
+
+    @GetMapping("/knife/create")
+    public String create(Model model) {
+        model.addAttribute("knife", new Knife());
+        return "knife/create";
+    }
+
+    @PostMapping("/knife/save")
+    public String postMethodName(@ModelAttribute("knife") Knife knife) {
+        knifeService.saveKnife(knife);
+        return "redirect:/knife";
     }
 }
