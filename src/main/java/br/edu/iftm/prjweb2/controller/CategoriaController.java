75fa62cab7c8a5eefd147745853400af2910ac06
Mateus <mateustpinheiro@gmail.com>
2026-06-20 19:37:54 -0300

    Ajuste de mapping no controller de Categoria

--- src/main/java/br/edu/iftm/prjweb2/controller/CategoriaController.java
+++ src/main/java/br/edu/iftm/prjweb2/controller/CategoriaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.validation.BindingResult;
 
 import br.edu.iftm.prjweb2.model.Categoria;
@@ -14,24 +15,25 @@ import br.edu.iftm.prjweb2.service.CategoriaService;
 import jakarta.validation.Valid;
 
 @Controller
+@RequestMapping("/categoria")
 public class CategoriaController {
     
     @Autowired
     private CategoriaService categoriaService;
 
-    @GetMapping("/categoria")
+    @GetMapping
     public String index(Model model) {
-        model.addAttribute("categoriaList", categoriaService.getAllCategorias());
+        model.addAttribute("categorias", categoriaService.getAllCategorias());
         return "categoria/index";
     }
 
-    @GetMapping("/categoria/create")
+    @GetMapping("/create")
     public String create(Model model) {
         model.addAttribute("categoria", new Categoria());
         return "categoria/create";
     }
 
-    @PostMapping("/categoria/save")
+    @PostMapping("/save")
     public String save(@ModelAttribute @Valid Categoria categoria, BindingResult result, Model model) {
 
         if (result.hasErrors()) {
@@ -43,13 +45,13 @@ public class CategoriaController {
         return "redirect:/categoria";
     }
 
-    @GetMapping("/categoria/delete/{id}")
+    @GetMapping("/delete/{id}")
     public String delete(@PathVariable Long id) {
         this.categoriaService.deleteCategoriaById(id);
         return "redirect:/categoria";
     }
 
-    @GetMapping("/categoria/edit/{id}")
+    @GetMapping("/edit/{id}")
     public String edit(@PathVariable Long id, Model model) {
         Categoria categoria = categoriaService.getCategoriaById(id);
         model.addAttribute("categoria", categoria);
