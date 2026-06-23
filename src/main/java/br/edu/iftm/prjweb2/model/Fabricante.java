4c2186ff15e7b78885e083cf873e797ded0d9f08
Mateus <mateustpinheiro@gmail.com>
2026-06-23 12:58:46 -0300

    Correção de restrição na entidade Fabricante

--- src/main/java/br/edu/iftm/prjweb2/model/Fabricante.java
+++ src/main/java/br/edu/iftm/prjweb2/model/Fabricante.java
@@ -8,9 +8,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "fabricante")
@@ -28,7 +29,8 @@ public class Fabricante {
     @NotBlank(message = "O país de origem é obrigatório.")
     private String origem;
 
-    @PastOrPresent(message = "O ano de fundação não pode ser no futuro.")
+    @NotNull(message = "O ano de fundação é obrigatório.")
+    @Min(value = 1, message = "O ano deve ser no mínimo 1.")
     private Integer anoFundacao;
 
     @OneToMany(mappedBy = "fabricante")
