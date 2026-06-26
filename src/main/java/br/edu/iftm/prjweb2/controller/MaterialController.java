3cba577008cc213689ee2963e80de22fafc2b480
Mateus <mateustpinheiro@gmail.com>
2026-06-25 21:29:48 -0300

    Adição da classe de controle para Material

--- src/main/java/br/edu/iftm/prjweb2/controller/MaterialController.java
+++ src/main/java/br/edu/iftm/prjweb2/controller/MaterialController.java
@@ -10,52 +10,52 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.validation.BindingResult;
 
-import br.edu.iftm.prjweb2.model.Categoria;
-import br.edu.iftm.prjweb2.service.CategoriaService;
+import br.edu.iftm.prjweb2.model.Material;
+import br.edu.iftm.prjweb2.service.MaterialService;
 
 import jakarta.validation.Valid;
 
 @Controller
-@RequestMapping("/categoria")
-public class CategoriaController {
+@RequestMapping("/material")
+public class MaterialController {
     
     @Autowired
-    private CategoriaService categoriaService;
+    private MaterialService materialService;
 
     @GetMapping
     public String index(Model model) {
-        model.addAttribute("categorias", categoriaService.getAllCategorias());
-        return "categoria/index";
+        model.addAttribute("materials", materialService.getAllMaterials());
+        return "material/index";
     }
 
     @GetMapping("/create")
     public String create(Model model) {
-        model.addAttribute("categoria", new Categoria());
-        return "categoria/create";
+        model.addAttribute("material", new Material());
+        return "material/create";
     }
 
     @PostMapping("/save")
-    public String save(@ModelAttribute @Valid Categoria categoria, BindingResult result, Model model) {
+    public String save(@ModelAttribute @Valid Material material, BindingResult result, Model model) {
 
         if (result.hasErrors()) {
-            model.addAttribute("categoria", categoria);
-            return "categoria/create";
+            model.addAttribute("material", material);
+            return "material/create";
         }
 
-        categoriaService.saveCategoria(categoria);
-        return "redirect:/categoria";
+        materialService.saveMaterial(material);
+        return "redirect:/material";
     }
 
     @GetMapping("/delete/{id}")
     public String delete(@PathVariable Long id) {
-        this.categoriaService.deleteCategoriaById(id);
-        return "redirect:/categoria";
+        this.materialService.deleteMaterialById(id);
+        return "redirect:/material";
     }
 
     @GetMapping("/edit/{id}")
     public String edit(@PathVariable Long id, Model model) {
-        Categoria categoria = categoriaService.getCategoriaById(id);
-        model.addAttribute("categoria", categoria);
-        return "categoria/edit";
+        Material material = materialService.getMaterialById(id);
+        model.addAttribute("material", material);
+        return "material/edit";
     }
 }
